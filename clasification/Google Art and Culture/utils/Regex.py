87c5e217ab47a07e2cb70639b40b24c688d549e8
alejomaar <demonmaar@gmail.com>
2022-08-04 11:12:59 -0500

    Add regex functions, documentations and unit test

--- clasification/Google Art and Culture/utils/Regex.py
+++ clasification/Google Art and Culture/utils/Regex.py
@@ -1,11 +1,36 @@
 import re
+
 class Regex():
     def find(self,regex,string):
         try:
-            return re.findall(regex, string)[0]
+            pattern = re.compile(regex, re.VERBOSE)
+            return pattern.findall(string)[0]
         except:
             return None
 
-    def get_digit(self,string):
+    def get_integer(self,string):
         return self.find("\d+",string)
+
+    def get_width(self,string):
+        pattern = (
+        "(?<=w)"        #Positive lookbehind of w
+        "("
+            "\d+.\d+"    #Decimal numbers
+            "|\d+"       #Integer numbers
+        ")"
+        )
+        return self.find(pattern,string)
+    
+    def get_height(self,string):
+        pattern = (
+        "(?<=h)"         #Positive lookbehind of h
+        "("
+            "\d+.\d+"    #Decimal numbers
+            "|\d+"       #Integer numbers
+        ")"
+        )
+        return self.find(pattern,string)
+    
+    def get_measure(self,string):
+        return self.find("(cm|mm|in)",string)
 
--- clasification/Google Art and Culture/utils/test/Regex_test.py
+++ clasification/Google Art and Culture/utils/test/Regex_test.py
@@ -3,11 +3,34 @@ from utils.Regex import Regex
 
 regex =Regex()
 class Regex_Test(unittest.TestCase):
-    def test_get_digit(self):     
-        self.assertEqual(regex.get_digit('4545'), '4545')
-        self.assertEqual(regex.get_digit('454$^y5fg'), '454')
-        self.assertEqual(regex.get_digit('454 between string 789'), '454')
+    def test_get_integer(self):    
+        #Capture the number when it exist 
+        self.assertEqual(regex.get_integer('4545'), '4545')
+        self.assertEqual(regex.get_integer('4545,78'), '4545')
+        self.assertEqual(regex.get_integer('454$^y5fg'), '454')
+        self.assertEqual(regex.get_integer('454 between string 789'), '454')
+        #return None when number does not exist
+        self.assertEqual(regex.get_integer('only string'), None)      
 
-    def test_get_digit_failure(self):
-        regex =Regex()
-        self.assertEqual(regex.get_digit('only string'), None)
+    def test_get_width(self):     
+        #Capture the width when it exist
+        self.assertEqual(regex.get_width('w100'),'100')
+        self.assertEqual(regex.get_width('h100 w200'),'200')
+        self.assertEqual(regex.get_width('h100 w200.47'),'200.47')
+        self.assertEqual(regex.get_width('h100 w500,47'),'500,47')
+        #return None when width does not exist
+        self.assertEqual(regex.get_width('h100'),None)
+    
+    def test_get_height(self):     
+        #Capture the height when it exist
+        self.assertEqual(regex.get_height('h100'),'100')
+        self.assertEqual(regex.get_height('w100 h200'),'200')
+        self.assertEqual(regex.get_height('w100 h200.47'),'200.47')
+        self.assertEqual(regex.get_height('w100 h500,47'),'500,47')
+        #return None when height does not exist
+        self.assertEqual(regex.get_height('w100'),None)
+    
+    def test_get_measure(self):     
+        #Capture the height when it exist
+        self.assertEqual(regex.get_measure('w100 cm h100cm'),'cm')
+        self.assertEqual(regex.get_measure('100 200 mm'),'mm')
