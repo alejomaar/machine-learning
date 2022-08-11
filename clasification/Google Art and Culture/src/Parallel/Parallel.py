4f3f7b6abd9ebfb15947e8f470dd6f2fcfe402ae
alejomaar <demonmaar@gmail.com>
2022-08-10 23:06:05 -0500

    OPTIMISE parallel function. TEST paralell

--- clasification/Google Art and Culture/src/Parallel/Parallel.py
+++ clasification/Google Art and Culture/src/Parallel/Parallel.py
@@ -1,25 +1,10 @@
 from ast import arg
 from concurrent.futures import ThreadPoolExecutor
 
+def parallel(fun,values):
+    result =[]
+    with ThreadPoolExecutor(max_workers=None) as exe: 
+        print('Running')
+        result = exe.map(fun,values)
+    return list(result)
 
-'''def fun(x):
-    return x+1
-
-lista = [1,2,3,4,5,6,7,8,9,10]
-
-
-result =[]
-with ThreadPoolExecutor(max_workers=3) as exe: 
-    print('Running')
-    result = exe.map(fun,lista)
-
-print(list(result))
-print('End')'''
-
-def myfun(a,b):
-    return a+b+1
-
-def parallel(fun,args):
-    return fun(*args)
-
-print(parallel(myfun,(5,10)))
---
+++ clasification/Google Art and Culture/src/Parallel/test/Parallel_test.py
@@ -0,0 +1,13 @@
+import unittest
+from .. import parallel
+
+
+
+class Parallell_Test(unittest.TestCase):
+    def test_parallel(self):    
+        def plus_one(x):
+            return x+1
+        lista = [1,2,3,4,5]
+
+        new_list = parallel(plus_one,lista)
+        self.assertEqual(new_list, [2,3,4,5,6])   
