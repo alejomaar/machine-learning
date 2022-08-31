fe2555a45064336b9044d4c6fe6c376fb9b4232f
CrIstianBeltV <cristianbeltvel@gmail.com>
2022-08-30 22:09:21 -0500

    Add autor

--- clasification/Google Art and Culture/setup.py
+++ clasification/Google Art and Culture/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='google-images',
     version='0.1.0',
-    author='alejandro_aponte',
+    author='alejandro_aponte, cristian_beltran',
     description='description',
     python_requires='>=3',
     license='MIT',
